befcf65ef40e9a7c79fe303cf5dec740157d3cb7
Tarun Nagarajan <tarun.greenville@gmail.com>
2025-11-21 08:48:27 +0530

    kernel(relu): simple and efficient 1D stride

--- playlist/relu/02_single_strided.cu
+++ playlist/relu/02_single_strided.cu
@@ -2,9 +2,10 @@
 
 __global__ void relukern(const float* input, float* output, size_t totalsize) {
     size_t idx = blockDim.x * blockIdx.x + threadIdx.x;
-    
-    if (idx < totalsize) {
-        output[idx] = fmaxf(0.0f, input[idx]); 
+    size_t stride = blockDim.x * gridDim.x;
+
+    for (size_t i = idx; i < totalsize; i += stride) {
+        output[i] = fmaxf(0.0f, input[i]);
     }
 }
 
@@ -12,7 +13,9 @@ extern "C" void solution(const float* input, float* output, size_t n, size_t m) 
     size_t totalsize = m * n; 
 
     int threads = 256;
-    int blocks = (totalsize + threads - 1) / threads;
+    int sm_count;
+    cudaDeviceGetAttribute(&sm_count, cudaDevAttrMultiProcessorCount, 0);
+    int blocks = sm_count * 32;
     
     relukern<<<blocks, threads>>>(input, output, totalsize);
 }
